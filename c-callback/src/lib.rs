412b5968672bba503ba716a2f14bc0020602566c
Kristopher Wuollett <kristopher.wuollett@gmail.com>
2023-07-19 12:14:52 -0500

    fix: hide disjoint field with fn

--- c-callback/src/lib.rs
+++ c-callback/src/lib.rs
@@ -4,52 +4,32 @@ async fn init_val_fn() -> i32 {
     1
 }
 
-struct StdMutexCallback {
-    context: std::sync::Arc<std::sync::Mutex<*mut std::os::raw::c_void>>,
-    f: extern "C" fn(context: *mut std::os::raw::c_void),
+struct Callback {
+    context: *mut std::os::raw::c_void,
+    on_complete: extern "C" fn(context: *mut std::os::raw::c_void),
 }
 
-unsafe impl Send for StdMutexCallback {}
-
-struct TokioMutexCallback {
-    context: std::sync::Arc<tokio::sync::Mutex<*mut std::os::raw::c_void>>,
-    f: extern "C" fn(context: *mut std::os::raw::c_void),
+impl Callback {
+    fn context_as_ptr(&self) -> *mut std::os::raw::c_void {
+        self.context
+    }
 }
 
-unsafe impl Send for TokioMutexCallback {}
+unsafe impl Send for Callback {}
 
 #[no_mangle]
-pub extern "C" fn std_mutex_init(
+pub extern "C" fn app_init(
     context: *mut std::os::raw::c_void,
     on_complete: extern "C" fn(context: *mut std::os::raw::c_void),
 ) {
-    let callback = StdMutexCallback {
-        context: std::sync::Arc::new(std::sync::Mutex::new(context)),
-        f: on_complete,
+    let callback = Callback {
+        context,
+        on_complete,
     };
     tokio::task::spawn(async move {
         println!("init task");
         let val = VAL.get_or_init(init_val_fn).await;
         println!("init done: {val}");
-        let mut locked_context = callback.context.lock().unwrap();
-        (callback.f)(*locked_context);
-    });
-}
-
-#[no_mangle]
-pub extern "C" fn tokio_mutex_init(
-    context: *mut std::os::raw::c_void,
-    on_complete: extern "C" fn(context: *mut std::os::raw::c_void),
-) {
-    let callback: TokioMutexCallback = TokioMutexCallback {
-        context: std::sync::Arc::new(tokio::sync::Mutex::new(context)),
-        f: on_complete,
-    };
-    tokio::task::spawn(async move {
-        println!("init task");
-        let val = VAL.get_or_init(init_val_fn).await;
-        println!("init done: {val}");
-        let mut locked_context = callback.context.lock().await;
-        (callback.f)(*locked_context);
+        (callback.on_complete)(callback.context_as_ptr());
     });
 }
